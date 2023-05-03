6b225ac1a645a9b004f17f3116aedc30189f8ac5
Oliver <oliver22official@gmail.com>
2023-05-03 23:51:36 +0200

    contraseña actviada

---
+++ app/administrador/administradorContrasena.js
@@ -0,0 +1,27 @@
+const claveDeAcceso = "Albertito";/*LA CLAVE DE ACCESO */
+
+const customAlert = document.getElementById('custom-alert');/*coge el elemento de la alerta, se escribe fuera para que se pueda usar en ambos metodos*/
+
+const input = document.getElementById('contrasena');/*Coge el elemento del input para luego crearle un evento(al persionar enter tambien entre)*/
+
+input.addEventListener('keydown', function(event) {/*un evento creado cada vez que pulsas una tecla en el input*/
+    if (event.key === 'Enter') {/*Te compara si la tecla q pulsas es el enter */
+      entrarAdmin();/*Si es igual al enter llama al metodo de entrarAdmin que o te manda a la pagina o te muestra la alerta */
+    }
+  });
+
+function entrarAdmin() {
+    var valor = document.getElementById("contrasena").value;/*te coge el valor que hay en el input, se pone dentro para coger el valor al pulsar el boton y no antes*/
+    if(valor == claveDeAcceso){/*te compara si tu contraseña es la correcta*/
+        location.href = `administradorCatalogo.html`;/*Te manda la alerta*/
+    }
+    else{
+        customAlert.style.display = 'block';/*Esto muestra la alerta*/
+    }
+}
+
+function hideAlert() {/*Para ocultar la alerta*/
+    customAlert.style.display = 'none';
+  }
+
+  
