c742d39b30f129eb86ee694b62f32517a6ba9d71
Veronica alejandra toro gomez <114920562+Veronicatorogomez@users.noreply.github.com>
2023-05-06 02:50:31 +0200

    Arreglo del codigo de LOCALSTORAGE que iba regulin ya va perfest !

--- app/contacto/contacto.js
+++ app/contacto/contacto.js
@@ -1,24 +1,34 @@
-//coger los elementos del html por id
 
-console.log(1);
-function cogerDatos(){
+let idNum = parseInt(localStorage.getItem("idNum")) || 0; // Tenemos que poner esto por que necesitamos guardar el numero por el que iba la id en
+// nuestro localStorage y si no hay ninguno coge el 0 pa que no de fallo.
+
+function crearID(){//Esto me crea el id de la propiedad id de mensaje ABAJO!!!
+    idNum++;
+    localStorage.setItem("idNum",idNum); //Me sube el incremento de id
+    return idNum;
+}
+
+function cogerDatos(){ //cojo los datos mediante su id, el valor!!!
 
     const valorEmail = document.getElementById("email").value;
     const valorAsunto = document.getElementById("asunto").value;
     const valorContenidoMensaje = document.getElementById("contenidoMensaje").value;
     
-    const mensaje = {
+    const mensaje = { // Creo un objeto para meter todos los datos despues de golpe!!!
+        id : crearID(),
         email : valorEmail,
         asunto : valorAsunto,
         contenidoMensaje :valorContenidoMensaje
     };
+    
+   
     return mensaje;
 }
 
 function enviarDatos(){
     
-    const mensaje = cogerDatos();
-    const mensajeJson = JSON.stringify(mensaje);
-    localStorage.setItem(mensaje.email,mensajeJson);
+    const mensaje = cogerDatos(); // Llamo el metodo donde hago lo de agrupar los datos.
+    const mensajeJson = JSON.stringify(mensaje); //Los convierto en un archivo Json
+    localStorage.setItem(mensaje.id,mensajeJson); // Los meto al local storage con un id
     
 }
