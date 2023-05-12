0731049c9fcea396a162c762137fa01ec12e93b1
adrian <adrianlm1985@gmail.com>
2023-05-12 11:17:39 +0200

    Metido datos de nombre, precio e imágenes
    He insertado todo eso, me falta ponerla en su tamaño ahora lo pongo y  subo. He tenido que tocar el json online para que coja las imágenes

--- app/Catalogo/Catalogo.js
+++ app/Catalogo/Catalogo.js
@@ -1,17 +1,29 @@
 const urlCasa = "https://getpantry.cloud/apiv1/pantry/464bfeaa-daae-4ecd-81c0-3a675193d25e/basket/Casas";
 
 let nombre = document.getElementById("nombre"); 
+const container = document.querySelector(".gridCatalogo");
 
 async function cogerCasas(){
     let res = await fetch(urlCasa), 
     json = await res.json();
     console.log(json.casas);
-    for(i = 0; i < 9; i++)
+    for(i = 1; i < 8; i++)
     {
-        console.log(json.casas[i]);
+        // console.log(json.casas[i]);
+        const casa = json.casas[i];
+        const card = document.createElement("div");
+        // card.classList.add("xx");
+        card.innerHTML = `
+        <button class="boton">
+            <div id="fotos" class="contenedorFotosYTexto"> 
+                <img src="${casa.imagenes.primero}">
+                <div class="texto grid-container">
+                    <span class="name">${casa.nombre}</span>
+                    <p>${casa.precio}</p>
+                </div>
+            </div>
+        </button>`;
+        container.appendChild(card);
     }
-        // nombre.innerHTML = element.nombre; 
-        //  console.log(dato.nombre);
-    
 }
 cogerCasas();
