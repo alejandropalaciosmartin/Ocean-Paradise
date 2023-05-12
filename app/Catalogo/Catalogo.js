569f94c807fe1b8a00119160b8c103b87c164978
adrian <adrianlm1985@gmail.com>
2023-05-12 13:18:47 +0200

    Enlazado catalogo con productos

--- app/Catalogo/Catalogo.js
+++ app/Catalogo/Catalogo.js
@@ -7,12 +7,15 @@ async function cogerCasas(){
     let res = await fetch(urlCasa), 
     json = await res.json();
     console.log(json.casas);
-    for(i = 1; i < 8; i++)
+    for(i = 1; i < 8; i++) //Recorremos 8 porque son los artículos que tenemos actualmente
     {
         // console.log(json.casas[i]);
-        const casa = json.casas[i];
+        const casa = json.casas[i]; //Metemos en una variable para simplificar la escritura
+        //Crea un botón
         const card = document.createElement("button");
+        //Ponemos efectos al botón que lo coge del Catalogo.css
          card.classList.add("boton");
+         //Dibujamos cada casa con sus efectos css que se coge del Catalogo.css
         card.innerHTML = `
             <div id="fotos" class="contenedorFotosYTexto"> 
                 <img src="${casa.imagenes[0]}" alt="fotoCasa" class="estiloFotos">
@@ -21,7 +24,8 @@ async function cogerCasas(){
                     <p>${casa.precio}</p>
                 </div>
             </div>`;
-
+        // Manda id a la otra web
+        card.addEventListener('click', () => { window.location.href = `../Producto/Producto.html?id=${casa.id}`; });
         container.appendChild(card);
     }
 }
