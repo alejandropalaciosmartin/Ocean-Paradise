679f63b4a7c35fe94c577bbd7005b2e1b19bcefa
Albertsak <albertog300604@gmail.com>
2023-05-14 19:35:07 +0200

    Local Storage 2

--- app/Producto/Producto.js
+++ app/Producto/Producto.js
@@ -1,35 +1,3 @@
-let cantidad = 1;
-
-function botonmas(){
-   cantidad= cantidad + 1;
-
-   document.querySelector("#cantidad").innerHTML= cantidad
-   if( cantidad < 1){
-    document.querySelector("#cantidad").innerHTML= 1
-
-    document.querySelector("#cantidad").innerHTML= 1
-   }
-}
-
-function botonmenos(){
-    if( cantidad < 1){
-      
-        document.querySelector("#cantidad").innerHTML= 1
-    }else
-    cantidad = cantidad -1;
-
-    document.querySelector("#cantidad").innerHTML= cantidad
-
-  
-}
-
-function realizarcompra(){
-
-    window.location.href = ".//app/CarritoCompra/CarritoCompra.html";
-
-}
-
-
 /* LOCALSTORAGE */
 var elementoNombre = document.getElementById("nombre");
 var contenidoNombre = elementoNombre.innerHTML;
@@ -48,6 +16,51 @@ localStorage.setItem("cantidad", contenidoCantidad);
 
 
 let datos = [contenidoPrecio, contenidoCantidad, contenidoNombre]
+
+let cantidad = 1; /* ESTA CANTIDAD NO ES LA MISMA QUE LA DEL LOCALSTORAGE*/
+
+function botonmas(){
+   cantidad= cantidad + 1;
+
+   document.querySelector("#cantidad").innerHTML= cantidad
+   if( cantidad < 1){
+    document.querySelector("#cantidad").innerHTML= 1
+
+    document.querySelector("#cantidad").innerHTML= 1
+   }
+
+   contenidoCantidad +1;
+   
+   
+}
+
+function botonmenos(){
+    if( cantidad < 1){
+      
+        document.querySelector("#cantidad").innerHTML= 1
+    }else
+    cantidad = cantidad -1;
+
+    document.querySelector("#cantidad").innerHTML= cantidad
+
+  
+}
+
+function realizarcompra(){
+
+    window.location.href = "file:///C:/Users/Albertsack/Documents/GitHub/Ocean-Paradise/app/CarritoCompra/CarritoCompra.html";
+
+}
+
+
+function confirmarcantidad()/* ESTA FUNCIÓN SIRVE PARA QUE EL EN LOCAL STORAGE SE ALMACENE LA CANTIDAD DEL PRODUCTO SEECCIONADO*/
+{
+    localStorage.setItem("cantidad", cantidad)
+
+}
+
+
+
 
 
 
