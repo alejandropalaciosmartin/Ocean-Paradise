1f85f8666d0d60ce408365d8ce838d5a973ae62f
Albertsak <albertog300604@gmail.com>
2023-05-16 11:17:48 +0200

    sessionstorage acabado

--- app/Producto/Producto.js
+++ app/Producto/Producto.js
@@ -1,91 +1,132 @@
-/* LOCALSTORAGE */
 const enlace = window.location.search;
-
 const urlparametros = new URLSearchParams(enlace);
+const id = urlparametros.get("id");
 
-const id = urlparametros.get("id"); 
+const elementoNombre = document.getElementById("nombre");
+const contenidoNombre = elementoNombre.innerHTML;
 
+const elementoPrecio = document.getElementById("precio");
+const contenidoPrecio = elementoPrecio.innerHTML;
 
+const elementoCantidad = document.getElementById("cantidad");
+let cantidadd = 1;
 
-var elementoNombre = document.getElementById("nombre");
-var contenidoNombre = elementoNombre.innerHTML;
-
-
-var elementoPrecio = document.getElementById("precio");
-var contenidoPrecio = elementoPrecio.innerHTML;
-
-
-
-var elementoCantidad = document.getElementById("cantidad");
-var contenidoCantidad= elementoCantidad.innerHTML;
-
-const datos = [contenidoPrecio, contenidoCantidad, contenidoNombre]
-
-
-
-
-
-
-
-
-
-let cantidad = 1; /* ESTA CANTIDAD NO ES LA MISMA QUE LA DEL LOCALSTORAGE*/
-
-function botonmas(){
-   cantidad= cantidad + 1;
-
-   document.querySelector("#cantidad").innerHTML= cantidad
-   if( cantidad < 1){
-    document.querySelector("#cantidad").innerHTML= 1
-
-    document.querySelector("#cantidad").innerHTML= 1
-   }
-
-   contenidoCantidad +1;
-   
-   
+// Verificar si hay un valor almacenado en el sessionStorage
+const datosGuardados = sessionStorage.getItem(id);
+if (datosGuardados) {
+  const datos = JSON.parse(datosGuardados);
+  cantidadd = datos[2];
+  elementoCantidad.innerHTML = cantidadd;
 }
 
-function botonmenos(){
-    if( cantidad < 1){
-      
-        document.querySelector("#cantidad").innerHTML= 1
-    }else
-    cantidad = cantidad -1;
+// Manejar el evento beforeunload para guardar los datos antes de abandonar la página
+window.addEventListener("beforeunload", function() {
+  const datos = [contenidoNombre, contenidoPrecio, cantidadd];
+  sessionStorage.setItem(id, JSON.stringify(datos));
+});
 
-    document.querySelector("#cantidad").innerHTML= cantidad
+// Restablecer la cantidad a 1 al volver a la página
+document.addEventListener("DOMContentLoaded", function() {
+  elementoCantidad.innerHTML = 1;
+});
 
-  
+function botonmas() {
+  cantidadd += 1;
+  elementoCantidad.innerHTML = cantidadd;
 }
 
-function realizarcompra(){
-
-    window.location.href = "file:///C:/Users/Albertsack/Documents/GitHub/Ocean-Paradise/app/CarritoCompra/CarritoCompra.html";
-
+function botonmenos() {
+  if (cantidadd > 1) {
+    cantidadd -= 1;
+    elementoCantidad.innerHTML = cantidadd;
+  }
 }
 
+function realizarcompra() {
+  window.location.href = "file:///C:/Users/Albertsack/Documents/GitHub/Ocean-Paradise/app/CarritoCompra/CarritoCompra.html";
+}
 
-function confirmarcantidad()/* ESTA FUNCIÓN SIRVE PARA QUE EL EN LOCAL STORAGE SE ALMACENE LA CANTIDAD DEL PRODUCTO SEECCIONADO*/
-{
-    var elementoCantidad = document.getElementById("cantidad");
-    var contenidoCantidad= elementoCantidad.innerHTML;
+function añadirCarrito() {
+  var contenidoCantidad = parseInt(elementoCantidad.innerText);
 
-    const datos = [contenidoNombre,contenidoPrecio, contenidoCantidad ]
+  if (sessionStorage.getItem(id) == null) {
+    const datos = [contenidoNombre, contenidoPrecio, contenidoCantidad];
+    sessionStorage.setItem(id, JSON.stringify(datos));
+  } else {
+    const datosGuardados = sessionStorage.getItem(id);
+    const datos = JSON.parse(datosGuardados);
+    const cantidadAnterior = datos[2];
+    const cantidadFinal = cantidadAnterior + contenidoCantidad;
+    datos[2] = cantidadFinal;
+    sessionStorage.setItem(id, JSON.stringify(datos));
+  }
 
-    sessionStorage.setItem(id, datos)
-
+  // Aquí puedes agregar cualquier otra lógica relacionada con añadir al carrito
 }
 
 
 
+/*const enlace = window.location.search;
+const urlparametros = new URLSearchParams(enlace);
+const id = urlparametros.get("id");
 
+const elementoNombre = document.getElementById("nombre");
+const contenidoNombre = elementoNombre.innerHTML;
 
+const elementoPrecio = document.getElementById("precio");
+const contenidoPrecio = elementoPrecio.innerHTML;
 
+const elementoCantidad = document.getElementById("cantidad");
+let cantidadd = 1;
 
+// Verificar si hay un valor almacenado en el sessionStorage
+const datosGuardados = sessionStorage.getItem(id);
+if (datosGuardados) {
+  const datos = JSON.parse(datosGuardados);
+  cantidadd = datos[2];
+  elementoCantidad.innerHTML = cantidadd;
+}
 
+// Manejar el evento beforeunload para guardar los datos antes de abandonar la página
+window.addEventListener("beforeunload", function() {
+  const datos = [contenidoNombre, contenidoPrecio, cantidadd];
+  sessionStorage.setItem(id, JSON.stringify(datos));
+});
 
+// Restablecer la cantidad a 1 al volver a la página
+document.addEventListener("DOMContentLoaded", function() {
+  elementoCantidad.innerHTML = 1;
+});
 
+function botonmas() {
+  cantidadd += 1;
+  elementoCantidad.innerHTML = cantidadd;
+}
 
-  
+function botonmenos() {
+  if (cantidadd > 1) {
+    cantidadd -= 1;
+    elementoCantidad.innerHTML = cantidadd;
+  }
+}
 
+function realizarcompra() {
+  window.location.href = "file:///C:/Users/Albertsack/Documents/GitHub/Ocean-Paradise/app/CarritoCompra/CarritoCompra.html";
+}
+
+function confirmarcantidad() {
+  var contenidoCantidad = parseInt(elementoCantidad.innerText);
+
+  if (sessionStorage.getItem(id) == null) {
+    const datos = [contenidoNombre, contenidoPrecio, contenidoCantidad];
+    sessionStorage.setItem(id, JSON.stringify(datos));
+  } else {
+    const datosGuardados = sessionStorage.getItem(id);
+    const datos = JSON.parse(datosGuardados);
+    const cantidadAnterior = datos[2];
+    const cantidadFinal = cantidadAnterior + contenidoCantidad;
+    datos[2] = cantidadFinal;
+    sessionStorage.setItem(id, JSON.stringify(datos));
+  }
+}*/
 
