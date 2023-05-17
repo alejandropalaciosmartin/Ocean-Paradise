7e895b90d0cfd268f970485322129993e57a7155
Oliver <oliver22official@gmail.com>
2023-05-17 21:37:48 +0200

    carrito vinculao

--- app/CarritoCompra/CarritoCompra.js
+++ app/CarritoCompra/CarritoCompra.js
@@ -1,3 +1,7 @@
+//constante de la url de la api
+const urlCasa = "https://getpantry.cloud/apiv1/pantry/464bfeaa-daae-4ecd-81c0-3a675193d25e/basket/Casas";
+CogerDatos();
+
 //Función para actualizar el número del carrito
 function actualizarNumeroCarrito(numero) {
     const elementoNumero = document.getElementById('sumaTotalCarrito');
@@ -47,3 +51,40 @@ function botonmenos(){
   
 }
 
+//metiendo datitos al carrito
+
+async function CogerDatos(){
+  let res = await fetch(urlCasa), 
+    json = await res.json();
+    
+    const bodyTable = document.getElementById("tbody")
+
+    for (let i = 0; i < sessionStorage.length; i++) { // Recorrer todas las claves en el Session Storage
+      const key = sessionStorage.key(i); // Obtener la clave actual
+      const value = JSON.parse(sessionStorage.getItem(key)); // Obtener el valor correspondiente a la clave 
+      
+      const tr = document.createElement("tr");
+      tr.innerHTML = 
+      `<td><img src="${json.casas[key].imagenes[0]}"></td>
+      <td>${json.casas[key].nombre}</td>
+      <td>
+        <div class="contador">
+            <button class="boton-mas" onclick="botonmas(${key})" id="botonsumar">+</button> <!--Para que te obtenga del producto que tengo, le tengo que meter en los parámetros la id del producto-->
+            <p id="cantidad" class="numero" min="1">${value[2]}</p>
+            <button class="boton-menos" onclick="botonmenos(${key})" id="botonrestar">-</button>
+        </div>
+      </td>
+        <td >
+        <input class="inputprecio" id="precio" value="${json.casas[key].precio}" readonly>
+        </td>
+      <td id="subtotal"></td>`;
+      bodyTable.appendChild(tr);
+    }
+    if(sessionStorage.length == 0){
+      const tr = document.createElement("tr");
+      tr.innerHTML = 
+      `<td colspan="5" id="sinProductos" >NO HAY PRODUCTOS EN LA CESTA</td>`;
+      bodyTable.appendChild(tr);
+    }
+
+}
