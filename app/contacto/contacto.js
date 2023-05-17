a3f9b4ffc8acf42c5d9e903e633485d51fb1aac4
adrian <adrianlm1985@gmail.com>
2023-05-17 17:56:11 +0200

    Contacto flecha arriba puesta

--- app/contacto/contacto.js
+++ app/contacto/contacto.js
@@ -31,3 +31,11 @@ function enviarDatos(){
     localStorage.setItem(mensaje.id,mensajeJson); // Los meto al local storage con un id
     
 }
+
+//Flecha ir arriba
+window.onscroll = function() {
+    const myBtn = document.getElementById("botonFlecha");
+    myBtn.style.display = (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) ? "block" : "none"; };
+    
+  function topFunction() {
+    document.documentElement.scrollTop = 0; }
