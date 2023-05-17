1cdb4b9f64661819b3408c4d00d566cd242d8c19
Alejandro Palacios Martín <srjalean@gmail.com>
2023-05-17 23:49:59 +0200

    boton mas y menos del carrito ya funcionan creo

--- app/CarritoCompra/CarritoCompra.js
+++ app/CarritoCompra/CarritoCompra.js
@@ -20,36 +20,7 @@ function actualizarNumeroCarrito(numero) {
   }
   actualizarNumeroCarrito(sumaTotal);
   
-  //--------------------------------------------------------------------------------------------------------------------
-
-let cantidad = 1;
-let precio = parseInt(document.getElementById("precio").value);
-
-function botonmas(){
-   cantidad= cantidad + 1;
-
-   document.querySelector("#cantidad").innerHTML= cantidad
-   if( cantidad < 1){
-    cantidad= cantidad + 0;
-
-    document.querySelector("#cantidad").innerHTML= 1
-   }
-
-   document.querySelector("#subtotal").innerHTML = precio * cantidad + "€"
-}
-
-function botonmenos(){
-    if( cantidad < 1){
-        cantidad = cantidad -0;
-        document.querySelector("#cantidad").innerHTML= 1
-    }else
-    cantidad = cantidad -1;
-
-    document.querySelector("#cantidad").innerHTML= cantidad
-    document.querySelector("#subtotal").innerHTML = precio * cantidad + "€"
-
-  
-}
+//--------------------------------------------------------------------------------------------------------------------
 
 //metiendo datitos al carrito
 
@@ -64,12 +35,13 @@ async function CogerDatos(){
       const value = JSON.parse(sessionStorage.getItem(key)); // Obtener el valor correspondiente a la clave 
       
       const tr = document.createElement("tr");
+      tr.id = key;
       tr.innerHTML = 
       `<td><img src="${json.casas[key].imagenes[0]}"></td>
       <td>${json.casas[key].nombre}</td>
       <td>
         <div class="contador">
-            <button class="boton-mas" onclick="botonmas(${key})" id="botonsumar">+</button> <!--Para que te obtenga del producto que tengo, le tengo que meter en los parámetros la id del producto-->
+            <button class="boton-mas" onclick="botonmas(${key})" id="botonsumar">+</button> 
             <p id="cantidad" class="numero" min="1">${value[2]}</p>
             <button class="boton-menos" onclick="botonmenos(${key})" id="botonrestar">-</button>
         </div>
@@ -80,11 +52,86 @@ async function CogerDatos(){
       <td id="subtotal"></td>`;
       bodyTable.appendChild(tr);
     }
+
     if(sessionStorage.length == 0){
       const tr = document.createElement("tr");
+
       tr.innerHTML = 
       `<td colspan="5" id="sinProductos" >NO HAY PRODUCTOS EN LA CESTA</td>`;
       bodyTable.appendChild(tr);
     }
+}
 
+
+
+
+
+//  let precio = parseInt(document.getElementById("precio").value);
+
+function botonmas(key){
+    const value = JSON.parse(sessionStorage.getItem(key));
+    let cantidad = value[2];
+    cantidad += 1;
+
+   document.querySelector("#cantidad").innerHTML= cantidad
+   if( cantidad < 1){
+    document.querySelector("#cantidad").innerHTML= 1
+   }
+
+  const datosGuardados = sessionStorage.getItem(key);
+  const datos = JSON.parse(datosGuardados);
+  datos[2] = cantidad;
+  sessionStorage.setItem(key, JSON.stringify(datos));
+
+  //Código para calcular la cantidad total guardada en el SessionStorage para poner el numero en el carrito de la compra
+    
+  let sumaTotal = 0; // Inicializar la variable para almacenar la suma total
+  
+  for (let i = 0; i < sessionStorage.length; i++) { // Recorrer todas las claves en el Session Storage
+    const key = sessionStorage.key(i); // Obtener la clave actual
+    const value = JSON.parse(sessionStorage.getItem(key)); // Obtener el valor correspondiente a la clave 
+    const cantidad = value[2]; // Obtener la cantidad del valor y sumarla a la suma total
+    sumaTotal += cantidad;
+  }
+  actualizarNumeroCarrito(sumaTotal);
+
+  //  document.querySelector("#subtotal").innerHTML = precio * cantidad + "€"
+
+}
+
+  
+function botonmenos(key){
+  const value = JSON.parse(sessionStorage.getItem(key));
+  let cantidad = value[2];
+  cantidad -= 1;
+  document.querySelector("#cantidad").innerHTML= cantidad
+  let element = document.getElementById(key);
+  if( cantidad < 1){
+    sessionStorage.removeItem(key);
+    element.remove();
+    const tbody = document.getElementById("tbody");
+    tbody.innerHTML = "";
+    CogerDatos();
+  }
+else{
+  const datosGuardados = sessionStorage.getItem(key);
+  const datos = JSON.parse(datosGuardados);
+  datos[2] = cantidad;
+  sessionStorage.setItem(key, JSON.stringify(datos));
+}
+  
+
+  //Código para calcular la cantidad total guardada en el SessionStorage para poner el numero en el carrito de la compra
+  
+  let sumaTotal = 0; // Inicializar la variable para almacenar la suma total
+
+  for (let i = 0; i < sessionStorage.length; i++) { // Recorrer todas las claves en el Session Storage
+    const key = sessionStorage.key(i); // Obtener la clave actual
+    const value2 = JSON.parse(sessionStorage.getItem(key)); // Obtener el valor correspondiente a la clave 
+    const cantidad2 = value2[2]; // Obtener la cantidad del valor y sumarla a la suma total
+    sumaTotal += cantidad2;
+  }
+  actualizarNumeroCarrito(sumaTotal);
+
+  //  document.querySelector("#subtotal").innerHTML = precio * cantidad + "€"
 }
