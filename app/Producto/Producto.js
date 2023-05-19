97a3147561ada0c12000ea239a352f7a9e5efcfb
Albertsak <albertog300604@gmail.com>
2023-05-19 12:02:50 +0200

    seesion completado ya

--- app/Producto/Producto.js
+++ app/Producto/Producto.js
@@ -1,3 +1,131 @@
+const enlace = window.location.search;
+const urlparametros = new URLSearchParams(enlace);
+const id = urlparametros.get("id");
+
+// Función para actualizar el número del carrito
+function actualizarNumeroCarrito(numero) {
+  const elementoNumero = document.getElementById('sumaTotalCarrito');
+  elementoNumero.textContent = numero;
+}
+
+// Código para calcular la cantidad total guardada en el SessionStorage para poner el número en el carrito de la compra
+let sumaTotal = 0; // Inicializar la variable para almacenar la suma total
+
+for (let i = 0; i < sessionStorage.length; i++) { // Recorrer todas las claves en el Session Storage
+  const key = sessionStorage.key(i); // Obtener la clave actual
+  const value = JSON.parse(sessionStorage.getItem(key)); // Obtener el valor correspondiente a la clave 
+  const cantidad = value[2]; // Obtener la cantidad del valor y sumarla a la suma total
+  sumaTotal += cantidad;
+}
+
+actualizarNumeroCarrito(sumaTotal);
+
+//--------------------------------------------------------------------------------------------------------------------
+
+fetch('https://getpantry.cloud/apiv1/pantry/464bfeaa-daae-4ecd-81c0-3a675193d25e/basket/Casas')
+  .then(response => response.json())
+  .then(data => {
+    const div = document.createElement('div');
+    document.getElementById("foto").src = data.casas[id].imagenes[0];
+    document.getElementById("imagen1").src = data.casas[id].imagenes[1];
+    document.getElementById("imagen2").src = data.casas[id].imagenes[2];
+    document.getElementById("imagen3").src = data.casas[id].imagenes[3];
+    document.getElementById("imagen4").src = data.casas[id].imagenes[4];
+
+    document.getElementById("descripcion").innerHTML = data.casas[id].descripcion;
+
+    const nombreCasa = data.casas[id].nombre;
+    const precioCasa = data.casas[id].precio;
+
+    // Guardar los valores en sessionStorage
+    const datos = [nombreCasa, precioCasa, 1]; // La cantidad inicial se establece en 1
+    sessionStorage.setItem(id, JSON.stringify(datos));
+
+    // Actualizar los elementos HTML con los valores obtenidos
+    document.getElementById("nombre").innerHTML = nombreCasa;
+    document.getElementById("precio").innerHTML = precioCasa;
+  });
+
+const elementoNombre = document.getElementById("nombre");
+const contenidoNombre = elementoNombre.innerHTML;
+
+const elementoPrecio = document.getElementById("precio");
+const contenidoPrecio = elementoPrecio.innerHTML;
+
+const elementoCantidad = document.getElementById("cantidad");
+let cantidadd = 1;
+
+// Verificar si hay un valor almacenado en el sessionStorage
+const datosGuardados = sessionStorage.getItem(id);
+if (datosGuardados) {
+  const datos = JSON.parse(datosGuardados);
+  cantidadd = datos[2];
+  elementoCantidad.innerHTML = cantidadd;
+}
+
+// Restablecer la cantidad a 1 al volver a la página
+document.addEventListener("DOMContentLoaded", function() {
+  elementoCantidad.innerHTML = 1;
+});
+
+function botonmas() {
+  let cantidadd2 = parseInt(elementoCantidad.innerHTML);
+  cantidadd2 += 1;
+  elementoCantidad.innerHTML = cantidadd2;
+}
+
+function botonmenos() {
+  let cantidadd2 = parseInt(elementoCantidad.innerHTML);
+  if (cantidadd2 > 1) {
+    cantidadd2 -= 1;
+    elementoCantidad.innerHTML = cantidadd2;
+  }
+}
+
+function realizarcompra() {
+  window.location.href = "../CarritoCompra/CarritoCompra.html";
+}
+
+function añadirCarrito() {
+  const contenidoCantidad = parseInt(elementoCantidad.innerText);
+
+  if (sessionStorage.getItem(id) == null) {
+    const datos = [contenidoNombre, contenidoPrecio, contenidoCantidad];
+    sessionStorage.setItem(id, JSON.stringify(datos));
+  } else {
+    const datosGuardados = sessionStorage.getItem(id);
+    const datos = JSON.parse(datosGuardados);
+    const cantidadAnterior = datos[2];
+    const cantidadFinal = cantidadAnterior + contenidoCantidad;
+    datos[2] = cantidadFinal;
+    sessionStorage.setItem(id, JSON.stringify(datos));
+  }
+
+  // Código para calcular la cantidad total guardada en el SessionStorage para poner el número en el carrito de la compra
+  let sumaTotal = 0; // Inicializar la variable para almacenar la suma total
+
+  for (let i = 0; i < sessionStorage.length; i++) { // Recorrer todas las claves en el Session Storage
+    const key = sessionStorage.key(i); // Obtener la clave actual
+    const value = JSON.parse(sessionStorage.getItem(key)); // Obtener el valor correspondiente a la clave 
+    const cantidad = value[2]; // Obtener la cantidad del valor y sumarla a la suma total
+    sumaTotal += cantidad;
+  }
+  actualizarNumeroCarrito(sumaTotal);
+}
+
+// Flecha ir arriba
+window.onscroll = function() {
+  const myBtn = document.getElementById("botonFlecha");
+  myBtn.style.display = (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) ? "block" : "none";
+};
+
+function topFunction() {
+  document.documentElement.scrollTop = 0;
+}
+/*const enlace = window.location.search;
+const urlparametros = new URLSearchParams(enlace);
+const id = urlparametros.get("id");
+
 //Función para actualizar el número del carrito
 function actualizarNumeroCarrito(numero) {
   const elementoNumero = document.getElementById('sumaTotalCarrito');
@@ -16,18 +144,56 @@ for (let i = 0; i < sessionStorage.length; i++) { // Recorrer todas las claves e
 }
 actualizarNumeroCarrito(sumaTotal);
 
+
+
+
+
+
 //--------------------------------------------------------------------------------------------------------------------
+fetch('https://getpantry.cloud/apiv1/pantry/464bfeaa-daae-4ecd-81c0-3a675193d25e/basket/Casas')
+  .then(response => response.json())
+  .then(data => {
+    
+    const div = document.createElement('div')
+    document.getElementById("foto").src= data.casas[id].imagenes[0];
+    document.getElementById("imagen1").src = data.casas[id].imagenes[1];
+    document.getElementById("imagen2").src = data.casas[id].imagenes[2];
+    document.getElementById("imagen3").src = data.casas[id].imagenes[3];
+    document.getElementById("imagen4").src = data.casas[id].imagenes[4];
+
+    document.getElementById("descripcion").innerHTML = data.casas[id].descripcion;
 
 
 
 
-/* que la cantidad de los botones sea igual a la cantidad final*/
-const enlace = window.location.search;
-const urlparametros = new URLSearchParams(enlace);
-const id = urlparametros.get("id");
+
+  div.innerHTML = 
+
+
+  `<div class="texto">
+                   
+  <p id="nombre">${document.getElementById("nombre").innerHTML = data.casas[id].nombre}</p>
+  <p id="precio">${document.getElementById("precio").innerHTML = data.casas[id].precio}</p>
+ 
+</div>`;
+ 
+
+
+ 
+  })
+
+
+
+
+
 
 const elementoNombre = document.getElementById("nombre");
-const contenidoNombre = elementoNombre.innerHTML;
+
+const contenidoNombre =  elementoNombre.innerHTML;
+
+
+
+
 
 const elementoPrecio = document.getElementById("precio");
 const contenidoPrecio = elementoPrecio.innerHTML;
@@ -106,36 +272,7 @@ function topFunction() {
   document.documentElement.scrollTop = 0; }
   
 
-  fetch('https://getpantry.cloud/apiv1/pantry/464bfeaa-daae-4ecd-81c0-3a675193d25e/basket/Casas')
-  .then(response => response.json())
-  .then(data => {
-    
-    const div = document.createElement('div')
-    document.getElementById("foto").src= data.casas[id].imagenes[0];
-    document.getElementById("imagen1").src = data.casas[id].imagenes[1];
-    document.getElementById("imagen2").src = data.casas[id].imagenes[2];
-    document.getElementById("imagen3").src = data.casas[id].imagenes[3];
-    document.getElementById("imagen4").src = data.casas[id].imagenes[4];
-
-    document.getElementById("descripcion").innerHTML = data.casas[id].descripcion;
-
-
-
-
-
-  div.innerHTML = 
-  /*`<div >
-  <img id="foto" src="${ console.log(data.casas[id].imagenes[0])}" alt="imagen" class="imagen">
-</div>`*/
-
-
-  `<div class="texto">
-                   
-  <p id="nombre">${document.getElementById("nombre").innerHTML = data.casas[id].nombre}</p>
-  <p id="precio">${document.getElementById("precio").innerHTML = data.casas[id].precio}</p>
- 
-</div>`
-  })
+  
 
  
-
+*/
