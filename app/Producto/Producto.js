e373f301d19657273a1221e398e1ed2de1115286
Albertsak <albertog300604@gmail.com>
2023-05-19 19:15:47 +0200

    ¿se acabó producto?
    ale no me digas nada  :)

--- app/Producto/Producto.js
+++ app/Producto/Producto.js
@@ -31,6 +31,152 @@ fetch('https://getpantry.cloud/apiv1/pantry/464bfeaa-daae-4ecd-81c0-3a675193d25e
     document.getElementById("imagen2").src = data.casas[id].imagenes[2];
     document.getElementById("imagen3").src = data.casas[id].imagenes[3];
     document.getElementById("imagen4").src = data.casas[id].imagenes[4];
+    document.getElementById("imagen5").src = data.casas[id].imagenes[0];
+
+    document.getElementById("descripcion").innerHTML = data.casas[id].descripcion;
+
+    const nombreCasa = data.casas[id].nombre;
+    const precioCasa = data.casas[id].precio;
+
+    // Actualizar los elementos HTML con los valores obtenidos
+    document.getElementById("nombre").innerHTML = nombreCasa;
+    document.getElementById("precio").innerHTML = precioCasa;
+  });
+
+function cambiofotos(event) {
+  const imagenSeleccionada = event.target.src;
+  document.getElementById("foto").src = imagenSeleccionada;
+}
+
+// Asignar la función cambiofotos() a los botones de imagen
+document.getElementById("botonImagen1").addEventListener("click", cambiofotos);
+document.getElementById("botonImagen2").addEventListener("click", cambiofotos);
+document.getElementById("botonImagen3").addEventListener("click", cambiofotos);
+document.getElementById("botonImagen4").addEventListener("click", cambiofotos);
+document.getElementById("botonImagen5").addEventListener("click", cambiofotos);
+
+const elementoNombre = document.getElementById("nombre");
+const contenidoNombre = elementoNombre.innerHTML;
+
+const elementoPrecio = document.getElementById("precio");
+const contenidoPrecio = elementoPrecio.innerHTML;
+
+const elementoCantidad = document.getElementById("cantidad");
+let cantidad = 1;
+
+// Verificar si hay un valor almacenado en el sessionStorage
+const datosGuardados = sessionStorage.getItem(id);
+if (datosGuardados) {
+  const datos = JSON.parse(datosGuardados);
+  cantidad = datos[2];
+  elementoCantidad.innerHTML = cantidad;
+}
+
+// Restablecer la cantidad a 1 al volver a la página
+document.addEventListener("DOMContentLoaded", function() {
+  elementoCantidad.innerHTML = 1;
+});
+
+function botonmas() {
+  cantidad++;
+  elementoCantidad.innerHTML = cantidad;
+}
+
+function botonmenos() {
+  if (cantidad > 1) {
+    cantidad--;
+    elementoCantidad.innerHTML = cantidad;
+  }
+}
+
+function realizarcompra() {
+  window.location.href = "../CarritoCompra/CarritoCompra.html";
+}
+
+function añadirCarrito() {
+  const contenidoCantidad = parseInt(elementoCantidad.innerText);
+
+  if (sessionStorage.getItem(id) == null) {
+    const datos = [contenidoNombre, contenidoPrecio, contenidoCantidad];
+    sessionStorage.setItem(id, JSON.stringify(datos));
+
+    // Código para calcular la cantidad total guardada en el SessionStorage para poner el número en el carrito de la compra
+    sumaTotal += contenidoCantidad;
+    actualizarNumeroCarrito(sumaTotal);
+  } else {
+    const datosGuardados = sessionStorage.getItem(id);
+    const datos = JSON.parse(datosGuardados);
+    const cantidadAnterior = datos[2];
+    const cantidadFinal = cantidadAnterior + contenidoCantidad;
+    datos[2] = cantidadFinal;
+    sessionStorage.setItem(id, JSON.stringify(datos));
+
+    // Código para actualizar la cantidad total en el carrito de la compra
+    sumaTotal += contenidoCantidad;
+    actualizarNumeroCarrito(sumaTotal);
+  }
+}
+
+// Flecha ir arriba
+window.onscroll = function() {
+  const myBtn = document.getElementById("botonFlecha");
+  myBtn.style.display = (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) ? "block" : "none";
+};
+
+function topFunction() {
+  document.documentElement.scrollTop = 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*Este el codigo bueno 18:06
+const enlace = window.location.search;
+const urlparametros = new URLSearchParams(enlace);
+const id = urlparametros.get("id");
+
+// Función para actualizar el número del carrito
+function actualizarNumeroCarrito(numero) {
+  const elementoNumero = document.getElementById('sumaTotalCarrito');
+  elementoNumero.textContent = numero;
+}
+
+// Código para calcular la cantidad total guardada en el SessionStorage para poner el número en el carrito de la compra
+let sumaTotal = 0; // Inicializar la variable para almacenar la suma total
+
+for (let i = 0; i < sessionStorage.length; i++) { // Recorrer todas las claves en el Session Storage
+  const key = sessionStorage.key(i); // Obtener la clave actual
+  const value = JSON.parse(sessionStorage.getItem(key)); // Obtener el valor correspondiente a la clave 
+  const cantidad = value[2]; // Obtener la cantidad del valor y sumarla a la suma total
+  sumaTotal += cantidad;
+}
+
+actualizarNumeroCarrito(sumaTotal);
+
+//--------------------------------------------------------------------------------------------------------------------
+
+fetch('https://getpantry.cloud/apiv1/pantry/464bfeaa-daae-4ecd-81c0-3a675193d25e/basket/Casas')
+  .then(response => response.json())
+  .then(data => {
+    const div = document.createElement('div');
+    document.getElementById("foto").src = data.casas[id].imagenes[0];
+    document.getElementById("imagen1").src = data.casas[id].imagenes[1];
+    document.getElementById("imagen2").src = data.casas[id].imagenes[2];
+    document.getElementById("imagen3").src = data.casas[id].imagenes[3];
+    document.getElementById("imagen4").src = data.casas[id].imagenes[4];
+    document.getElementById("imagen5").src = data.casas[id].imagenes[0];
+  
+
 
     document.getElementById("descripcion").innerHTML = data.casas[id].descripcion;
 
@@ -45,6 +191,21 @@ fetch('https://getpantry.cloud/apiv1/pantry/464bfeaa-daae-4ecd-81c0-3a675193d25e
     document.getElementById("nombre").innerHTML = nombreCasa;
     document.getElementById("precio").innerHTML = precioCasa;
   });
+  
+
+  function cambiofotos(event) {
+    const imagenSeleccionada = event.target.src;
+    document.getElementById("foto").src = imagenSeleccionada;
+  }
+  
+  // Asignar la función cambiofotos() a los botones de imagen
+  document.getElementById("botonImagen1").addEventListener("click", cambiofotos);
+  document.getElementById("botonImagen2").addEventListener("click", cambiofotos);
+  document.getElementById("botonImagen3").addEventListener("click", cambiofotos);
+  document.getElementById("botonImagen4").addEventListener("click", cambiofotos);
+  document.getElementById("botonImagen5").addEventListener("click", cambiofotos);
+
+ 
 
 const elementoNombre = document.getElementById("nombre");
 const contenidoNombre = elementoNombre.innerHTML;
@@ -122,157 +283,7 @@ window.onscroll = function() {
 function topFunction() {
   document.documentElement.scrollTop = 0;
 }
-/*const enlace = window.location.search;
-const urlparametros = new URLSearchParams(enlace);
-const id = urlparametros.get("id");
-
-//Función para actualizar el número del carrito
-function actualizarNumeroCarrito(numero) {
-  const elementoNumero = document.getElementById('sumaTotalCarrito');
-  elementoNumero.textContent = numero;
-}
-
-//Código para calcular la cantidad total guardada en el SessionStorage para poner el numero en el carrito de la compra
-  
-let sumaTotal = 0; // Inicializar la variable para almacenar la suma total
-
-for (let i = 0; i < sessionStorage.length; i++) { // Recorrer todas las claves en el Session Storage
-  const key = sessionStorage.key(i); // Obtener la clave actual
-  const value = JSON.parse(sessionStorage.getItem(key)); // Obtener el valor correspondiente a la clave 
-  const cantidad = value[2]; // Obtener la cantidad del valor y sumarla a la suma total
-  sumaTotal += cantidad;
-}
-actualizarNumeroCarrito(sumaTotal);
 
 
 
-
-
-
-//--------------------------------------------------------------------------------------------------------------------
-fetch('https://getpantry.cloud/apiv1/pantry/464bfeaa-daae-4ecd-81c0-3a675193d25e/basket/Casas')
-  .then(response => response.json())
-  .then(data => {
-    
-    const div = document.createElement('div')
-    document.getElementById("foto").src= data.casas[id].imagenes[0];
-    document.getElementById("imagen1").src = data.casas[id].imagenes[1];
-    document.getElementById("imagen2").src = data.casas[id].imagenes[2];
-    document.getElementById("imagen3").src = data.casas[id].imagenes[3];
-    document.getElementById("imagen4").src = data.casas[id].imagenes[4];
-
-    document.getElementById("descripcion").innerHTML = data.casas[id].descripcion;
-
-
-
-
-
-  div.innerHTML = 
-
-
-  `<div class="texto">
-                   
-  <p id="nombre">${document.getElementById("nombre").innerHTML = data.casas[id].nombre}</p>
-  <p id="precio">${document.getElementById("precio").innerHTML = data.casas[id].precio}</p>
- 
-</div>`;
- 
-
-
- 
-  })
-
-
-
-
-
-
-const elementoNombre = document.getElementById("nombre");
-
-const contenidoNombre =  elementoNombre.innerHTML;
-
-
-
-
-
-const elementoPrecio = document.getElementById("precio");
-const contenidoPrecio = elementoPrecio.innerHTML;
-
-const elementoCantidad = document.getElementById("cantidad");
-let cantidadd = 1;
-
-// Verificar si hay un valor almacenado en el sessionStorage
-const datosGuardados = sessionStorage.getItem(id);
-if (datosGuardados) {
-  const datos = JSON.parse(datosGuardados);
-  cantidadd = datos[2];
-  elementoCantidad.innerHTML = cantidadd;
-}
-
-// Restablecer la cantidad a 1 al volver a la página
-document.addEventListener("DOMContentLoaded", function() {
-  elementoCantidad.innerHTML = 1;
-});
-
-function botonmas() {
-  let cantidadd2 = parseInt(elementoCantidad.innerHTML);
-  cantidadd2 += 1;
-  elementoCantidad.innerHTML = cantidadd2;
-}
-
-function botonmenos() {
-  let cantidadd2 = parseInt(elementoCantidad.innerHTML);
-  if (cantidadd2 > 1) {
-    cantidadd2 -= 1;
-    elementoCantidad.innerHTML = cantidadd2;
-  }
-}
-
-function realizarcompra() {
-  window.location.href = "../CarritoCompra/CarritoCompra.html";
-}
-
-function añadirCarrito() {
-  var contenidoCantidad = parseInt(elementoCantidad.innerText);
-
-  if (sessionStorage.getItem(id) == null) {
-    const datos = [contenidoNombre, contenidoPrecio, contenidoCantidad];
-    sessionStorage.setItem(id, JSON.stringify(datos));
-  } else {
-    const datosGuardados = sessionStorage.getItem(id);
-    const datos = JSON.parse(datosGuardados);
-    const cantidadAnterior = datos[2];
-    const cantidadFinal = cantidadAnterior + contenidoCantidad;
-    datos[2] = cantidadFinal;
-    sessionStorage.setItem(id, JSON.stringify(datos));
-  }
-
-  //Código para calcular la cantidad total guardada en el SessionStorage para poner el numero en el carrito de la compra
-  
-  let sumaTotal = 0; // Inicializar la variable para almacenar la suma total
-
-  for (let i = 0; i < sessionStorage.length; i++) { // Recorrer todas las claves en el Session Storage
-    const key = sessionStorage.key(i); // Obtener la clave actual
-    const value = JSON.parse(sessionStorage.getItem(key)); // Obtener el valor correspondiente a la clave 
-    const cantidad = value[2]; // Obtener la cantidad del valor y sumarla a la suma total
-    sumaTotal += cantidad;
-  }
-  actualizarNumeroCarrito(sumaTotal);
-  //--------------------------------------------------------------------------------------------------------------------
-
-  // Aquí puedes agregar cualquier otra lógica relacionada con añadir al carrito
-}
-
-//Flecha ir arriba
-window.onscroll = function() {
-  const myBtn = document.getElementById("botonFlecha");
-  myBtn.style.display = (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) ? "block" : "none"; };
-  
-function topFunction() {
-  document.documentElement.scrollTop = 0; }
-  
-
-  
-
- 
 */
