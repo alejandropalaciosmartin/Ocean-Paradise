13c10276bca9754fbe9689809d9a90f7e6c726bb
adrian <adrianlm1985@gmail.com>
2023-05-23 11:32:42 +0200

    Mano ratón y mensaje añadir producto

--- app/Producto/Producto.js
+++ app/Producto/Producto.js
@@ -121,6 +121,14 @@ function añadirCarrito() {
     if(elementoNumero.style.display == "none"){
       elementoNumero.style.display = "flex";
     }
+    // Seepalert2
+    Swal.fire({
+      position: 'center',
+      icon: 'success',
+      title: 'Compra realizada',
+      showConfirmButton: false,
+      timer: 1500
+    })
     actualizarNumeroCarrito(sumaTotal);
   }
 }
